5d40b598066f62a3c0790f4244fff18d06006327
cute-12 <57433031+cute-12@users.noreply.github.com>
2020-09-30 21:13:57 +0700

    Add files via upload
    Phiên bản từ điển command line sơ khai

--- Word.java
+++ Word.java
@@ -3,6 +3,8 @@ public class Word {
     private String word_explain;
 
     public Word(String word_target, String word_explain) {
+        this.word_explain = word_explain;
+        this.word_target = word_target;
     }
 
     public String getWord_target() {
